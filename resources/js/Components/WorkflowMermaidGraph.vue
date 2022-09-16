d24c0e87b9a536d86a3d5992632cb2b8c295a16e
Juan Pablo Barreto <juampi92@gmail.com>
2022-09-17 01:35:22 +0200

    Style mermaid

--- resources/js/Components/WorkflowMermaidGraph.vue
+++ resources/js/Components/WorkflowMermaidGraph.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="w-full h-full" ref="graph">{{ graph }}</div>
+    <div class="w-full h-full graph" ref="graph">
+        {{ graph }}
+    </div>
 </template>
 
 <script type="text/ecmascript-6">
 import mermaid from 'mermaid';
-import { definitionToMermaid, relationsToMermaid, statusToMermaid } from '../utils/mermaid';
+import {definitionToMermaid, relationsToMermaid, statusToMermaid} from '../utils/mermaid';
 
 export default {
     props: ['id', 'definition', 'relations', 'status'],
@@ -23,22 +25,10 @@ export default {
 
     computed: {
         graph() {
-            const styles = {
-                pending: 'stroke-dasharray: 5 5',
-                finished: 'fill:green',
-                processing: 'fill:yellow',
-                failed: 'fill:red',
-            };
-
-            console.log(this.definition, this.relations, this.status);
-
-            const markdown = `graph TD
+            return `graph TD
                 ${definitionToMermaid(this.definition)}
                 ${relationsToMermaid(this.relations)}
-                ${statusToMermaid(this.status, styles)}`;
-
-            console.log('graph', markdown);
-            return markdown;
+                ${statusToMermaid(this.status)}`;
         }
     },
 
